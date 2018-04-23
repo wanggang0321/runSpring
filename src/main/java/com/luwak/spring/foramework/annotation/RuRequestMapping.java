2750d01949296cb56a57e453c7cdef9d2885a490
wanggang0321 <578620382@qq.com>
2018-04-23 14:32:51 +0800

    1.第一步

---
+++ src/main/java/com/luwak/spring/foramework/annotation/RuRequestMapping.java
@@ -0,0 +1,21 @@
+package com.luwak.spring.foramework.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author wanggang
+ * @date 2018年4月23日 下午2:16:46
+ * 
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RuRequestMapping {
+	
+	String value() default "";
+	
+}
---
+++ src/main/java/com/luwak/spring/foramework/webmvc/servlet/DispatcherServlet.java
@@ -0,0 +1,12 @@
+package com.luwak.spring.foramework.webmvc.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author wanggang
+ * @date 2018年4月23日 下午2:29:56
+ * 
+ */
+public class DispatcherServlet extends HttpServlet {
+
+}
